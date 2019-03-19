d6aa7b2e67c9081737f68b7ab3d68f85e430320c
Rodrigo Torres <rodrigo@metaposto.com.br>
2019-03-19 16:25:20 -0300

    SellerService anda basic FindAll

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerservice;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerservice = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerservice.FindAll();
+            return View(list);
         }
     }
 }
